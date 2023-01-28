e13bf2c91ef14d4bbea2d532a5c66139e7755120
Idrak Mustahsin <playbox8g@gmail.com>
2023-01-28 18:56:10 -0500

    Landing page changes

--- remix/screens/Home.js
+++ remix/screens/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
   const navigation = useNavigation();
 
   return <Screen>
-    
+    <Text>Home</Text>
   </Screen>;
 }
 
--- remix/screens/Landing.js
+++ remix/screens/Landing.js
@@ -6,14 +6,20 @@ import { useNavigation } from '@react-navigation/native';
 import colors from '../config/colors';
 
 export default function Landing() {
-  const navigation = useNavigation();
+  const navigate = useNavigation().navigate;
+
+  const handleClick = () => {
+      navigate("Home");
+  }
 
   return <Screen 
   // backgroundImage={require('../assets/images/remix.png')}
   >
-    <View style={{flex: 1, width: "100%", marginTop: 50, alignItems: "center"}}>
-        <Image style={styles.logo} source={require('../assets/images/remix.png')}/>
-        <AppButton style={styles.btn} title='Get Started'/>
+    <View style={{flex: 1, width: "100%", marginTop: 50, justifyContent: "space-between", alignItems: "center"}}>
+        <View style={{backgroundColor: "black", padding: 30, borderRadius: "200%"}}>
+          <Image style={styles.logo} source={require('../assets/images/remix.png')}/>
+        </View>
+        <AppButton onPress={handleClick} style={styles.btn} title='Get Started'/>
     </View>
 
   </Screen>;
@@ -22,9 +28,11 @@ export default function Landing() {
 const styles = StyleSheet.create({
   btn: {
     width: '50%',
+    marginBottom: 100
   },
   logo: {
-    width: 200,
-    height: 200
+    width: 150,
+    height: 150,
+    tintColor: "white"
   },
 });
