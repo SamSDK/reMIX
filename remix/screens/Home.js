8648c415dc5278258a84a1f6c62a77c0a5a06b0e
Idrak Mustahsin <playbox8g@gmail.com>
2023-01-28 22:24:07 -0500

    Homepage changes

--- remix/screens/Home.js
+++ remix/screens/Home.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import AppButton from '../components/AppButton';
 import Screen from '../components/Screen';
 import { useNavigation } from '@react-navigation/native';
 import colors from '../config/colors';
+import { SearchBar } from 'react-native-elements';
 
 export default function Home() {
   const navigation = useNavigation();
 
   return <Screen>
-    <Text>Home</Text>
+    <SearchBar
+      placeholder="Type Here..."
+      lightTheme
+      // onChangeText={this.updateSearch}
+      // value={search}
+    />
+    <View style={{
+      flex: 1,
+      alignItems: "center",
+    }}>
+      <TouchableOpacity style={{
+          width: "90%",
+          padding: 30,
+          margin: 10,
+          borderRadius: 10,
+          backgroundColor: "white"
+        }}>
+        <Text style={{color: colors.darkGrey, fontSize: 18}}>Filters (0 applied)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{
+          width: "90%",
+          padding: 30,
+          margin: 10,
+          borderRadius: 10,
+          borderWidth: 3,
+          borderColor: colors.primary,
+          alignItems: "center"
+        }}>
+        <Text style={{fontWeight: "bold", color: colors.primary, fontSize: 24}}>Make your own recipe</Text>
+      </TouchableOpacity>
+      <View style={{padding: 20, paddingTop: 5, width: "100%"}}>
+        <Text style={{fontWeight: "bold", fontSize: 28, alignSelf: "flex-start"}}>Browse Recipes</Text>
+        <FlatList
+          // data={DATA}
+          // renderItem={({item}) => <Item title={item.title} />}
+          // keyExtractor={item => item.id}
+        />
+      </View>
+    </View>
   </Screen>;
 }
 
