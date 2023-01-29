e8ba9b63633918d28c789bb8df511b9d75cd31e2
Sam <35474951+SamSDK@users.noreply.github.com>
2023-01-29 00:17:51 -0500

    Added tags component

--- remix/screens/Landing.js
+++ remix/screens/Landing.js
@@ -9,30 +9,46 @@ export default function Landing() {
   const navigate = useNavigation().navigate;
 
   const handleClick = () => {
-      navigate("Home");
-  }
+    navigate('Home');
+  };
 
-  return <Screen 
-  backgroundImage={require('../assets/images/seamless.webp')}
-  >
-    <View style={{flex: 1, width: "100%", marginTop: 50, justifyContent: "space-between", alignItems: "center"}}>
-        <View style={{backgroundColor: "black", padding: 30, borderRadius: 200}}>
-          <Image style={styles.logo} source={require('../assets/images/remix.png')}/>
+  return (
+    <Screen backgroundImage={require('../assets/images/seamless.webp')}>
+      <View
+        style={{
+          flex: 1,
+          width: '100%',
+          marginTop: 50,
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <View
+          style={{ backgroundColor: 'black', padding: 30, borderRadius: 200 }}
+        >
+          <Image
+            style={styles.logo}
+            source={require('../assets/images/remix.png')}
+          />
         </View>
-        <AppButton onPress={handleClick} style={styles.btn} title='Get Started'/>
-    </View>
-
-  </Screen>;
+        <AppButton
+          onPress={handleClick}
+          style={styles.btn}
+          title="Get Started"
+        />
+      </View>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
   btn: {
     width: '50%',
-    marginBottom: 140
+    marginBottom: 140,
   },
   logo: {
     width: 200,
     height: 200,
-    tintColor: "white"
+    tintColor: 'white',
   },
 });
